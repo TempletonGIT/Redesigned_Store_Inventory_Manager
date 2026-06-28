7f8b677b7e99905df936280a4364965f0330649c
Zacharia-dipudi <230700659@mycput.ac.za>
2026-06-28 17:08:33 +0200

    Done with StoreFactory

---
+++ Redesigned_Store_Inventory_Manager/src/main/java/za/ac/cput/redesigned_store_inventory_manager/factory/StoreFactory.java
@@ -0,0 +1,17 @@
+package za.ac.cput.redesigned_store_inventory_manager.factory;
+
+import za.ac.cput.redesigned_store_inventory_manager.domain.Product;
+import za.ac.cput.redesigned_store_inventory_manager.domain.Store;
+
+public class StoreFactory {
+    public static Store createStore(String storeId, String storeName, String storePhoneNumber, String storeLocation, Product product) {
+
+        return new Store.Builder()
+                .setStoreId(storeId)
+                .setStoreName(storeName)
+                .setStorePhoneNumber(storePhoneNumber)
+                .setStoreLocation(storeLocation)
+                .setProduct(product)
+                .Build();
+    }
+}
